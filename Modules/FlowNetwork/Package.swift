1c4a4bab002ea5cd71dd6469c9fa174909ff1bbb
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2024-03-09 00:02:22 +0100

    Update example

--- Modules/FlowNetwork/Package.swift
+++ Modules/FlowNetwork/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		)
     ],
 	dependencies: [
-        .package(url: "https://github.com/gerardogrisolini/FlowKit.git", branch: "main")
+        .package(url: "https://github.com/gerardogrisolini/FlowKit.git", from: "1.0.0")
 	],
     targets: [
         .target(
