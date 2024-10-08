55b468edb96ca16ea31769b7b22d72023389926e
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2024-10-09 00:12:34 +0200

    Fix version

--- Modules/FlowShared/Package.swift
+++ Modules/FlowShared/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 		)
     ],
 	dependencies: [
-        .package(url: "https://github.com/gerardogrisolini/FlowKit.git", from: "1.0.3")
+        .package(url: "https://github.com/gerardogrisolini/FlowKit.git", from: "1.0.4")
 	],
     targets: [
         .target(
